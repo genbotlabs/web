959e522e4b16eb2b4518dc4a6f506a75c9767568
yugyeongh <uugyeong27@gmail.com>
2025-06-26 09:45:27 +0900

    [feat] add MainPage file

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Header from './components/Header/Header'
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
+    <App />
   </React.StrictMode>
 );
 
