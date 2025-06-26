5163bb81c94e81466cca74e044d9345cb8b1fee9
yugyeongh <uugyeong27@gmail.com>
2025-06-26 11:42:45 +0900

    [feat] add slideup message and change font

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -5,13 +5,13 @@ const MainSection = () => {
     const [messages, setMessages] = useState([]);
     const [index, setIndex] = useState(0);
     const messageQueue = [
-        { text: "A: 안녕하세요. Genbot의 문의봇입니다. 무엇을 도와드릴까요?", sender: 'bot' },
-        { text: "Q: 스터디룸 이용 가능 시간이 어떻게 돼?", sender: 'user' },
-        { text: "A: 24시간 이용 가능합니다!", sender: 'bot' },
-        { text: "Q: 스터디룸 안에 에어컨 있어?", sender: 'user' },
-        { text: "A: 네. 모든 스터디룸 안에 에어컨이 설치되어 있습니다. 만약 온도 조절을 희망하시면 카운터로 문의주세요.", sender: 'bot' },
-        { text: "Q: 그럼 음료 반입은 가능한가요?", sender: 'user' },
-        { text: "A: 네. 저희 Genbot 스터디룸은 음료 반입이 가능합니다. 다만, 재활용은 직접 해주셔야 합니다.", sender: 'bot' },
+        { text: "안녕하세요. Genbot의 문의봇입니다. 무엇을 도와드릴까요?", sender: 'bot' },
+        { text: "스터디룸 이용 가능 시간이 어떻게 돼?", sender: 'user' },
+        { text: "24시간 이용 가능합니다!", sender: 'bot' },
+        { text: "스터디룸 안에 에어컨 있어?", sender: 'user' },
+        { text: "네. 모든 스터디룸 안에 에어컨이 설치되어 있습니다. 만약 온도 조절을 희망하시면 카운터로 문의주세요.", sender: 'bot' },
+        { text: "그럼 음료 반입은 가능한가요?", sender: 'user' },
+        { text: "네. 저희 Genbot 스터디룸은 음료 반입이 가능합니다. 다만, 재활용은 직접 해주셔야 합니다.", sender: 'bot' },
     ];
 
     useEffect(() => {
