8b25e734591c169eb043edf915694888597afb36
yugyeongh <uugyeong27@gmail.com>
2025-06-30 10:45:48 +0900

    [feat] Detail 테이블 추가

--- backend/models.py
+++ backend/models.py
@@ -80,3 +80,18 @@ class Data(Base):
     detail_id = Column(Integer, ForeignKey("detail.detail_id", ondelete="CASCADE"), nullable=False)
 
     detail = relationship("Detail", back_populates="data_items")
+
+class Detail(Base):
+    __tablename__ = "detail"
+
+    detail_id = Column(Integer, primary_key=True, autoincrement=True)
+    company_name = Column(String, nullable=True)
+    usage = Column(String, nullable=True)
+    first_text = Column(String, nullable=True)
+    description = Column(String, nullable=True)
+    voicebot_id = Column(Integer, ForeignKey("voicebot.voicebot_id", ondelete="RESTRICT"), nullable=True)
+    chatbot_id = Column(Integer, ForeignKey("chatbot.chatbot_id", ondelete="RESTRICT"), nullable=True)
+
+    voicebot = relationship("Voicebot", backref="details")
+    chatbot = relationship("Chatbot", backref="details")
+    data_items = relationship("Data", back_populates="detail", cascade="all, delete-orphan")
