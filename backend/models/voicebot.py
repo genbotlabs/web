5a8228203081e45fb676d59f27850ee763ffac1f
yugyeongh <uugyeong27@gmail.com>
2025-06-30 11:10:42 +0900

    [feat] voicebot.py의 voicebot_url String 설정 추가하기

--- backend/models/voicebot.py
+++ backend/models/voicebot.py
@@ -7,7 +7,7 @@ class Voicebot(Base):
 
     voicebot_id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("user.user_id", ondelete="CASCADE"), nullable=False)
-    voicebot_url = Column(String, nullable=False, default="/api/webhook-url")
+    voicebot_url = Column(String(255), nullable=False, default="/api/webhook-url")
     created_at = Column(DateTime, nullable=False, server_default=func.now())
 
     user = relationship("User", back_populates="voicebots")
