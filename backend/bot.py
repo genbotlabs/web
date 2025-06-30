fe37e1e312733987bbef01e7917f6c1c8190a191
yugyeongh <uugyeong27@gmail.com>
2025-06-30 15:24:02 +0900

    [feat/#5] json 파일은 s3에 저장하기

--- backend/bot.py
+++ backend/bot.py
@@ -2,6 +2,8 @@ import os
 from fastapi import UploadFile, File, Form, APIRouter
 from typing import List
 
+from services.s3 import upload_json_to_s3
+
 router = APIRouter()
 UPLOAD_DIR = "./uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
@@ -10,7 +12,7 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 async def generate_bot(
     type: str = Form(...),
     company: str = Form(...),
-    purpose: str = Form(...),
+    usage: str = Form(...),
     greeting: str = Form(""),
     description: str = Form(""),
     files: List[UploadFile] = File(...)
@@ -20,13 +22,19 @@ async def generate_bot(
         file_path = os.path.join(UPLOAD_DIR, file.filename)
         with open(file_path, "wb") as f:
             f.write(contents)
+        
+        uploaded_urls = []
+        if file.filename.endswith(".json"):
+            url = upload_json_to_s3(file.file, file.filename)
+            uploaded_urls.append(url)
 
     return {
         "message": "Bot 생성 요청 수신 완료",
+        "uploaded_urls": uploaded_urls,
         "info": {
             "type": type,
             "company": company,
-            "purpose": purpose,
+            "usage": usage,
             "greeting": greeting,
             "description": description,
             "file_count": len(files)
