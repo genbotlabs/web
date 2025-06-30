683a400ab73af4037bc8ddca1b26d155df33aeda
yugyeongh <uugyeong27@gmail.com>
2025-06-30 11:10:14 +0900

    [feat] chatbot.py의 chatbot_url String 설정 추가하기

--- backend/models/chatbot.py
+++ backend/models/chatbot.py
@@ -7,7 +7,7 @@ class Chatbot(Base):
 
     chatbot_id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey("user.user_id", ondelete="CASCADE"), nullable=False)
-    chatbot_url = Column(String, nullable=False, default="https://localhost:3000")
+    chatbot_url = Column(String(255), nullable=False, default="https://localhost:3000")
     created_at = Column(DateTime, nullable=False, server_default=func.now())
 
     user = relationship("User", back_populates="chatbots")
