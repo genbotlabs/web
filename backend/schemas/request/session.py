6156afe3942ca89e9ad55704e5850a864dbbb5ce
Yugyeong <uugyeong27@gmail.com>
2025-07-14 21:15:05 +0900

    [feat] 메시지 전송 요청 메시지 타입 추가 #22

--- backend/schemas/request/session.py
+++ backend/schemas/request/session.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, Field
+from typing import Literal
 
 # 세션 생성 요청
 class CreateSessionRequest(BaseModel):
@@ -7,3 +8,4 @@ class CreateSessionRequest(BaseModel):
 # 메시지 전송 요청
 class SendMessageRequest(BaseModel):
     message: str = Field(..., example="화장실은 어디에 있나요?")
+    message_type: Literal["text", "voice"] = Field("text", example="text")
