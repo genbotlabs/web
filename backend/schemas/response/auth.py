1d4cf590a23bc1ead144a4b8aab601112459d200
Yugyeong <uugyeong27@gmail.com>
2025-07-14 19:47:58 +0900

    [feat] 회원 정보 조회  응답 스키마 추가 #22

--- backend/schemas/response/auth.py
+++ backend/schemas/response/auth.py
@@ -17,3 +17,6 @@ class LoginResponse(BaseModel):
     access_token: str = Field(..., example="access_token")
     refresh_token: str = Field(..., example="refresh_token")
     user: UserInfo
+
+class UserMeResponse(BaseModel):
+    user: UserInfo
