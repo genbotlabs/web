a7745bb7fc7efc6f0f80e89a784c6899e8a1b743
Yugyeong <uugyeong27@gmail.com>
2025-07-24 20:52:05 +0900

    [feat] mainpage 구현 #8

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from  'react-router-dom';
+import 'antd/dist/reset.css'; 
 
 import './App.css';
 import ChatbotPage from './pages/ChatbotPage';
--- chatbot/src/pages/MainPage.js
+++ chatbot/src/pages/MainPage.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Button } from 'antd';
+
 import '../styles/MainPage.css';
 import logo from '../icons/logo.png'
 
 export default function MainPage() {
 
-  return (
-    <div>
-    </div>
-  );
+    const handleClick = () => {
+        window.location.href = '/chatbot';
+    }
+
+    return (
+        <div className="main-container">
+            <div className="content-wrapper">
+                <div className="logo-section">
+                    <img src={logo} className="logo-icon" alt="GenBot"/>
+                    <div className="text-section">
+                        <h1 className="main-title">OOO의 OOO봇</h1>
+                        <p className="subtitle">궁금한 사항은 언제든지 문의해 주세요</p>
+                    </div>
+                </div>
+
+                <div className="button-section">
+                    <Button type="primary" className="link-chatbot-button" onClick={handleClick}>
+                        챗봇 시작하기
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 }
