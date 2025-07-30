b9c74b94939609c04911b472a81eb06ceb458424
yugyeongh <uugyeong27@gmail.com>
2025-07-30 09:50:44 +0900

    [feat] 런팟 주소 env로 이동 완료 #53

--- chatbot/src/pages/MainPage.js
+++ chatbot/src/pages/MainPage.js
@@ -5,6 +5,9 @@ import { useSearchParams, useNavigate } from 'react-router-dom';
 import '../styles/MainPage.css';
 import logo from '../icons/logo.png'
 
+const runpodUrl = process.env.REACT_APP_RUNPOD_URL;
+const apiUrl = process.env.REACT_APP_API_URL;
+
 export default function MainPage() {
     const [companyName, setCompanyName] = useState('GenBot');
     const [botName, setBotName] = useState('문의');
@@ -17,7 +20,7 @@ export default function MainPage() {
         const botId = searchParams.get('bot_id');
         console.log(botId)
         if (botId) {
-            fetch(`http://localhost:8000/bots/detail/${botId}`)
+            fetch(`${apiUrl}/bots/detail/${botId}`)
                 .then(res => {
                     if (!res.ok) throw new Error('봇 정보를 가져오는 데 실패했습니다.');
                     return res.json();
@@ -36,7 +39,7 @@ export default function MainPage() {
 
     const handleClick = async () => {
         const botId = searchParams.get("bot_id") || "a1";
-        const response = await fetch(`http://localhost:8000/chatbot/${botId}`, {
+        const response = await fetch(`${apiUrl}/chatbot/${botId}`, {
             method: "POST",
             body: JSON.stringify({ bot_id: botId })
         });
@@ -45,12 +48,12 @@ export default function MainPage() {
         console.log('sessionId', sessionId)
 
         // const res = await fetch(`http://localhost:8000/bots/bot_id/${botId}`);
-        const res = await fetch(`http://localhost:8000/bots/contact/${botId}`);
+        const res = await fetch(`${apiUrl}/bots/contact/${botId}`);
         const botData = await res.json();
         const { cs_number, email, detail_id } = botData;
         console.log(cs_number, email, detail_id)
 
-        const runpodRes = await fetch(`https://bynve3gvz0rw60-7860.proxy.runpod.net/load`, {
+        const runpodRes = await fetch(`${runpodUrl}/load`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ bot_id: botId, cs_number, email, detail_id})
